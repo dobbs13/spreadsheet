d159e65c36f944db842ce83e92a0c81d18b39f88
dobbs13 <fish.ragozin@gmail.com>
2022-11-13 23:12:59 +0700

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { ListStates } from './models/user';
+import { users } from './data/users';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
+
+  listStates: ListStates = {
+    income: true,
+    outcome: false,
+    loans: false,
+    investments: false,
+  }
+
+  clientsInfo = users;
   title = 'spreadsheet';
+
+  setActive(key: string) {
+    this.resetStates()
+    this.listStates[key] = true
+  }
+
+  resetStates() {
+    for(let state in this.listStates) {
+      this.listStates[state] = false
+    }
+  }
+
+  filteredByType(type: string) {
+    return this.clientsInfo.data.filter((client) => client.type === type )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabComponent } from './components/tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabComponent,
   ],
   imports: [
     BrowserModule,
